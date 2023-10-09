c90ba11c7f070afbfdb8e48c1bfa39dcb5842157
lamnaourm <mohammed.lamnaour@gmail.com>
2023-10-09 15:30:08 +0100

    partie 2

--- src/components/JeuDe.jsx
+++ src/components/JeuDe.jsx
@@ -10,9 +10,9 @@ export default class JeuDe extends Component {
     jouer() {
         const valeur = Math.floor(Math.random() * 6) + 1;
         if (valeur === this.props.cache)
-            this.setState({face: valeur, compteur: this.state.compteur + 1,  fin: true })
+            this.setState({ face: valeur, compteur: this.state.compteur + 1, fin: true })
         else
-            this.setState({face: valeur, compteur: this.state.compteur + 1 })
+            this.setState({ face: valeur, compteur: this.state.compteur + 1 })
     }
 
     initialiser() {
@@ -22,16 +22,22 @@ export default class JeuDe extends Component {
     render() {
         return (
             <div className='jeu'>
-                <img src={require('../images/jeude.jpeg')} />
-                <h1>Jeu de Dé...</h1>
+                <img src={require('../images/jeude.jpeg')} alt='logo' />
+                <h1>Jeu de Dé</h1>
                 <h2>face: {this.state.face} </h2>
+                {
+                    this.state.face === null ? 
+                    <img className='imgde' src={require('../images/facevide.jpeg')} alt="" />
+                    :
+                    <img className='imgde' src={require('../images/face'+this.state.face+'.jpeg')} alt="" />
+                }
                 <h2>nombre d'essais : {this.state.compteur}</h2>
-                {this.state.fin ? 
-                        <div>
-                            <p>Bravo vous avez trouvez la face cachée.....</p>
-                            <button onClick={() => this.initialiser()}>Initialiser</button> 
-                        </div> : 
-                        <button onClick={() => this.jouer()}>jouer</button>
+                {this.state.fin ?
+                    <div>
+                        <p>Bravo vous avez trouvez la face cachée.....</p>
+                        <button onClick={() => this.initialiser()}>Initialiser</button>
+                    </div> :
+                    <button onClick={() => this.jouer()}>jouer</button>
                 }
 
             </div>
