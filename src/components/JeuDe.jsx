c2dce698f68883eeb5e17b223f12bf7a3c046574
lamnaourm <mohammed.lamnaour@gmail.com>
2023-10-09 14:23:46 +0100

    jeu de

---
+++ src/components/JeuDe.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+export default class JeuDe extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { face: null, compteur: 0, fin: false };
+    }
+    jouer() {
+        const valeur = Math.floor(Math.random() * 6) + 1;
+        //completer le code
+    }
+    initialiser() {
+        this.setState({ face: null, compteur: 0, fin: false });
+    }
+    render() {
+        return (
+            <div>
+                <img src={require('../images/jeude.jpeg')} />
+                <h1>Jeu de Dé...</h1>
+                <h2>face:….. </h2>
+                <h2>nombre d'essais…..</h2>
+                <button onClick={() => this.jouer()}>jouer</button>
+                <p>Bravo vous avez trouvez la face cachée.....</p>
+                <button onClick={() => this.initialiser()}>Initialiser</button>
+            </div>
+        )
+    }
+}
