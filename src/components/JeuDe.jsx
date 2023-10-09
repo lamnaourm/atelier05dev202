80500243d68d1fa6c5454595bef219a42275f9b8
lamnaourm <mohammed.lamnaour@gmail.com>
2023-10-09 14:32:45 +0100

    style

--- src/components/JeuDe.jsx
+++ src/components/JeuDe.jsx
@@ -6,16 +6,19 @@ export default class JeuDe extends Component {
         super(props);
         this.state = { face: null, compteur: 0, fin: false };
     }
+
     jouer() {
         const valeur = Math.floor(Math.random() * 6) + 1;
         //completer le code
     }
+
     initialiser() {
         this.setState({ face: null, compteur: 0, fin: false });
     }
+
     render() {
         return (
-            <div>
+            <div className='jeu'>
                 <img src={require('../images/jeude.jpeg')} />
                 <h1>Jeu de Dé...</h1>
                 <h2>face:….. </h2>
